328af328d9b6f4d4468d18e0a3118a9e3fee72f0
FranckyPapaTango <rafaralahyf@yahoo.fr>
2025-09-18 00:23:18 +0300

    DATABASE.PY dans database.py
    -encodage direct de l'url avec mot de passe encodé : DATABASE_URL = "postgresql+psycopg2://root:root1A%40@127.0.0.1:5432/gestocol"
                engine = create_engine(DATABASE_URL)

--- backend/database.py
+++ backend/database.py
@@ -10,16 +10,22 @@ load_dotenv()
 
 # Récupération et encodage du mot de passe
 user = os.getenv("POSTGRES_USER", "root")
-password = quote_plus(os.getenv("POSTGRES_PASSWORD", ""))
+password = quote_plus(os.getenv("POSTGRES_PASSWORD", "root1A@"))
 host = os.getenv("POSTGRES_HOST", "localhost")
 port = os.getenv("POSTGRES_PORT", "5432")
 db = os.getenv("POSTGRES_DB", "gestocol")
 
 # Construction de l'URL compatible SQLAlchemy
-DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+# DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{db}"
 
 # Création de l'engine
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+# engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+
+# from sqlalchemy import create_engine
+
+DATABASE_URL = "postgresql+psycopg2://root:root1A%40@127.0.0.1:5432/gestocol"
+engine = create_engine(DATABASE_URL)
+
 
 # Fabrique de sessions
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
